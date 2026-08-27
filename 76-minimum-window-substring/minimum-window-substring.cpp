ec2e21f7263f0651076b3d38fefe2341d7b0885e
VishalRaut-vr <rautv6491@gmail.com>
2026-08-27 10:53:37 +0530

    Time: 6 ms (63.63%) | Memory: 11.4 MB (83.33%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -5,10 +5,10 @@ public:
         if(t.size() > s.size()) return "";
 
         unordered_map<char,int> m;
+
         for(char& ch: t){
             m[ch]++;
         }
-
         int i = 0, j = 0, start_i = 0;
         int reqcount = t.size();
         int minwindow = INT_MAX;
@@ -16,18 +16,18 @@ public:
         while(j < s.size()){
             char ch = s[j];
 
-            if(m[ch] > 0) reqcount--;
-
+            if(m[ch] > 0){
+                reqcount--;
+            }
             m[ch]--;
 
             while(reqcount == 0){
-                int curr = j-i+1;
-
+                int curr = j - i + 1;
                 if(minwindow > curr){
                     minwindow = curr;
                     start_i = i;
-                }
 
+                }
                 m[s[i]]++;
 
                 if(m[s[i]] > 0){
@@ -36,8 +36,7 @@ public:
                 i++;
             }
             j++;
-            
         }
-        return minwindow == INT_MAX ? "" :  s.substr(start_i, minwindow);
+        return minwindow == INT_MAX ? "" : s.substr(start_i, minwindow);
     }
 };
