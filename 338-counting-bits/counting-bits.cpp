b117e34bdd68bd9dd1ac98f1e696c131efc132cd
VishalRaut-vr <rautv6491@gmail.com>
2026-08-27 11:22:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (97.75%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
-    int solve(int x){
-        int count = 0;
-        
-        while(x != 0){
-            x = x & (x-1);
-            count++;
-        }
-        return count;
-    }
     vector<int> countBits(int n) {
-        vector<int> ans(n+1, 0);
+        vector<int> ans(n+1);
 
-        for(int i=0; i<=n; i++){
-            ans[i] = solve(i);
+        for(int i=1; i<=n; i++){
+            if(i % 2 == 0){
+                ans[i] = ans[i/2];
+            }else{
+                ans[i] = ans[i/2] + 1;
+            }
         }
         return ans;
     }
-};
+};    
+
+//     int solve(int x){
+//         int count = 0;
+        
+//         while(x != 0){
+//             x = x & (x-1);
+//             count++;
+//         }
+//         return count;
+//     }
+//     vector<int> countBits(int n) {
+//         vector<int> ans(n+1, 0);
+
+//         for(int i=0; i<=n; i++){
+//             ans[i] = solve(i);
+//         }
+//         return ans;
+//     }
+// };
