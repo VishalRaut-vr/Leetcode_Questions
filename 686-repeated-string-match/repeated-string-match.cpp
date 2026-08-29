e700ff06ebf1fd7fd87ae0e8529433cb3918d5e9
VishalRaut-vr <rautv6491@gmail.com>
2026-08-29 23:15:48 +0530

    Time: 3 ms (76.40%) | Memory: 9.8 MB (65.21%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.cpp
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -3,23 +3,21 @@ public:
     int repeatedStringMatch(string a, string b) {
         int count = 1;
         string repeated = a;
-        
-        // Keep repeating until length >= b.length()
-        while (repeated.length() < b.length()) {
+
+        while(repeated.length() < b.length()){
             repeated += a;
             count++;
         }
-        
-        // Check if b is substring
-        if (repeated.find(b) != string::npos) 
+
+        if(repeated.find(b) != string:: npos){
             return count;
-        
-        // Check one more time (edge case)
+        }
+
         repeated += a;
         count++;
-        if (repeated.find(b) != string::npos) 
+        if(repeated.find(b) != string :: npos){
             return count;
-        
+        }
         return -1;
     }
 };
