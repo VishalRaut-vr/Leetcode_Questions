e5d5acdb830e2697084bb96d7d38e694383ea603
VishalRaut-vr <rautv6491@gmail.com>
2026-09-01 18:37:11 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (75.75%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+
         int candidate = nums[0];
         int count = 0;
-
         for(auto& i: nums){
             if(i == candidate){
                 count++;
