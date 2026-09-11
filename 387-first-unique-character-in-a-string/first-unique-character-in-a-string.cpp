1a68d35dc1a1fa4016f62452261217abaacc7d0b
VishalRaut-vr <rautv6491@gmail.com>
2026-09-11 20:36:50 +0530

    Time: 3 ms (79.99%) | Memory: 14.5 MB (86.99%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -3,15 +3,15 @@ public:
     int firstUniqChar(string s) {
        int freq[26] = {0};
 
-       for(char& ch: s){
+        for(char& ch: s){
             freq[ch-'a']++;
-       }
+        }
 
-       for(int i=0; i<s.length(); i++){
+        for(int i=0; i<s.length(); i++){
             if(freq[s[i]-'a'] == 1){
                 return i;
             }
-       }
-       return -1;
+        }
+        return -1;
     }
 };
