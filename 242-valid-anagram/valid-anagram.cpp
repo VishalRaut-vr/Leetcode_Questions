274997f64a49a3be02388df53e49fcdc280b2696
VishalRaut-vr <rautv6491@gmail.com>
2026-09-11 13:44:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (75.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int> count(26,0);
+        vector<int> count(26, 0);
 
-        for(char&ch: s){
+        for(char& ch: s){
             count[ch-'a']++;
         }
-        for(char&ch: t){
+
+        for(char& ch: t){
             count[ch-'a']--;
         }
 
