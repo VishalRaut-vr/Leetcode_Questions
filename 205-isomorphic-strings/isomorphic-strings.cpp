c4087285b52605ef147b383a53a91692d3ed02cb
VishalRaut-vr <rautv6491@gmail.com>
2026-09-11 21:35:27 +0530

    Time: 5 ms (10.80%) | Memory: 9.4 MB (29.62%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,16 +1,42 @@
-//Index-Based
+//Two Maps
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int maps[256] = {0};
-        int mapt[256] = {0};
 
-        for(int i=0; i<s.size(); i++){
-            if(maps[s[i]] != mapt[t[i]]) return false;
+        if(s.length() != t.length()) return false;
 
-            maps[s[i]] = i+1;
-            mapt[t[i]] = i+1;
+        unordered_map<char,char> smap;
+        unordered_map<char,char> tmap;
+
+        for(int i=0; i<s.length(); i++){
+            char cs = s[i];
+            char ct = t[i];
+            
+            if(smap.count(cs) && smap[cs] != ct) return false;
+            if(tmap.count(ct) && tmap[ct] != cs) return false;
+
+            smap[cs] = ct;
+            tmap[ct] = cs;
+
         }
         return true;
     }
 };
+
+
+// //Index-Based
+// class Solution {
+// public:
+//     bool isIsomorphic(string s, string t) {
+//         int maps[256] = {0};
+//         int mapt[256] = {0};
+
+//         for(int i=0; i<s.size(); i++){
+//             if(maps[s[i]] != mapt[t[i]]) return false;
+
+//             maps[s[i]] = i+1;
+//             mapt[t[i]] = i+1;
+//         }
+//         return true;
+//     }
+// };
