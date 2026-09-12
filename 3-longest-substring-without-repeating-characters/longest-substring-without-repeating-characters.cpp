56484c9e34055da6cc5693fa13f469d086d6c6a3
VishalRaut-vr <rautv6491@gmail.com>
2026-09-12 13:43:31 +0530

    Time: 307 ms (17.10%) | Memory: 81.4 MB (18.59%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -3,18 +3,19 @@ public:
     int lengthOfLongestSubstring(string s) {
         
         int n = s.length();
-        int left = 0;
+
         unordered_set<int> st;
+        int left = 0;
+        
         int maxcount = 0;
 
-        for(int right=0; right<n; right++){
+        for(int right = 0; right < n; right++){
             while(st.find(s[right]) != st.end()){
                 st.erase(s[left]);
                 left++;
             }
             st.insert(s[right]);
             maxcount = max(maxcount, right-left+1);
-
         }
         return maxcount;
     }
