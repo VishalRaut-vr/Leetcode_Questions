27d2faea5640866a62b8fde1d01266507321082d
VishalRaut-vr <rautv6491@gmail.com>
2026-09-18 13:28:33 +0530

    Time: 54 ms (53.97%) | Memory: 54.3 MB (42.12%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -6,7 +6,7 @@
 var addTwoPromises = async function(promise1, promise2) {
     //Better approach
     const [value1, value2] = await Promise.all([promise1, promise2]);
-     return value1 + value2;
+    return value1 + value2; 
 
     //2. Simple 
     // const value1 = await promise1;
