2722fb4f038973294616730a24aceeb47af56584
VishalRaut-vr <rautv6491@gmail.com>
2026-09-21 13:01:22 +0530

    Time: 858 ms (7.54%) | Memory: 9.3 MB (77.54%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    //dp
-    bool solve(string& s, int i, int j) {
-        if (i >= j) {
+    // dp
+    bool solve(string& s, int i, int j){
+        if(i >= j){
             return true;
         }
-        
 
-        if (s[i] == s[j]) {
-            return solve(s, i + 1, j - 1);
+        if(s[i] == s[j]){
+            return solve(s, i+1, j-1);
         }
         return false;
     }
@@ -18,13 +17,12 @@ public:
 
         int maxlen = INT_MIN;
         int sp = 0;
-        //memset for dp
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (solve(s, i, j) == true) {
-                    if (j - i + 1 > maxlen) {
-                        maxlen = j - i + 1;
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                if(solve(s, i, j) == true){
+                    if(j-i+1 > maxlen){
+                        maxlen = j-i+1;
                         sp = i;
                     }
                 }
