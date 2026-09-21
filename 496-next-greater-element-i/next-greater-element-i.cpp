df477f1d8a4d1a987eae70fcd18d318cad428d59
VishalRaut-vr <rautv6491@gmail.com>
2026-09-21 09:02:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (38.11%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -8,7 +8,7 @@ public:
             while(s.size() > 0 && s.top() <= nums2[i]){
                 s.pop();
             }
-            
+
             if(s.empty()){
                 m[nums2[i]] = -1;
             }else{
@@ -23,4 +23,5 @@ public:
         }
         return ans;
     }
+
 };
