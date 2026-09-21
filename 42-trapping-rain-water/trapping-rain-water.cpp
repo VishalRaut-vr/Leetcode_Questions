989ffd1e6f7256870a78fa33bdf3867c7ce4df1c
VishalRaut-vr <rautv6491@gmail.com>
2026-09-21 09:20:21 +0530

    Time: 0 ms (100.00%) | Memory: 25.8 MB (99.73%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -7,7 +7,7 @@ public:
         int lmax = 0, rmax = 0;
         int ans = 0;
 
-        while(l < r){
+        while(l <= r){
             lmax = max(lmax, height[l]);
             rmax = max(rmax, height[r]);
 
