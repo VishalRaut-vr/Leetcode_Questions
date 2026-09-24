335510d2c6c2ade0528a6217ea8f9ff62280e187
VishalRaut-vr <rautv6491@gmail.com>
2026-09-24 21:21:49 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (17.11%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,51 +1,13 @@
 class Solution {
 public:
-    // for first position
-    int firstpos(vector<int>& nums, int target){
-        int left = 0; 
-        int right = nums.size()-1;
-        int ans = -1;
-
-        while(left <= right){
-            int mid = left + (right-left)/2;
-
-            if(nums[mid] == target){
-                ans = mid;
-                right = mid-1; //keep searching on left side for first pos
-            }else if(nums[mid] < target){
-                left = mid + 1;
-            }else{
-                right = mid-1;
-            }
-        }
-        return ans;
-    }
-    // for last position
-    int lastpos(vector<int>& nums, int target){
-        int left = 0; 
-        int right = nums.size()-1;
-        int ans = -1;
-
-        while(left <= right){
-            int mid = left + (right-left)/2;
-
-            if(nums[mid] == target){
-                ans = mid;
-                left = mid+1; // keep searching on right side for last pos
-            }else if(nums[mid] < target){
-                left = mid+1;
-            }else{
-                right = mid-1;
-            }
-        }
-        return ans;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        if(nums.empty()) return {-1,-1};
+        auto first = lower_bound(nums.begin(), nums.end(), target);
+        auto last = upper_bound(nums.begin(), nums.end(), target);
 
-        int first = firstpos(nums, target);
-        int last = lastpos(nums, target);
+        if(first == nums.end() || *first != target){
+            return {-1,-1};
+        }
 
-        return {first, last};
+        return {(int)(first-nums.begin()), (int)(last-nums.begin()-1)};
     }
 };
